1eb00502371c9d08317c92cc1686e8a445036494
jat123123 <bhavishyarajput752@gmail.com>
2026-02-28 00:46:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.utils import platform
 from kivymd.toast import toast
+from kivy.core.clipboard import Clipboard
 
 if platform == "android":
     from jnius import autoclass, PythonJavaClass, java_method
@@ -30,6 +31,10 @@ if platform == "android":
 
 class RewardApp(MDApp):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ad = None   # 🔥 important (crash avoid)
+
     def build(self):
         return Builder.load_string(KV)
 
@@ -39,40 +44,59 @@ class RewardApp(MDApp):
 
     @run_on_ui_thread
     def init_ads(self):
-        MobileAds = autoclass('com.google.android.gms.ads.MobileAds')
-        activity = autoclass('org.kivy.android.PythonActivity').mActivity
-        MobileAds.initialize(activity)
+        try:
+            MobileAds = autoclass('com.google.android.gms.ads.MobileAds')
+            activity = autoclass('org.kivy.android.PythonActivity').mActivity
+            MobileAds.initialize(activity)
 
-        self.load_ad()
+            self.load_ad()
+
+        except Exception as e:
+            Clipboard.copy("INIT ERROR:\n" + str(e))
 
     @run_on_ui_thread
     def load_ad(self):
-        RewardedAd = autoclass(
-            'com.google.android.gms.ads.rewarded.RewardedAd'
-        )
-        AdRequest = autoclass(
-            'com.google.android.gms.ads.AdRequest$Builder'
-        )
-        activity = autoclass(
-            'org.kivy.android.PythonActivity'
-        ).mActivity
+        try:
+            RewardedAd = autoclass(
+                'com.google.android.gms.ads.rewarded.RewardedAd'
+            )
+            AdRequest = autoclass(
+                'com.google.android.gms.ads.AdRequest$Builder'
+            )
+            activity = autoclass(
+                'org.kivy.android.PythonActivity'
+            ).mActivity
 
-        self.ad = RewardedAd(
-            activity,
-            "ca-app-pub-3940256099942544/5224354917"
-        )
+            self.ad = RewardedAd(
+                activity,
+                "ca-app-pub-3940256099942544/5224354917"
+            )
 
-        self.ad.loadAd(AdRequest().build())
+            self.ad.loadAd(AdRequest().build())
+
+        except Exception as e:
+            Clipboard.copy("LOAD ERROR:\n" + str(e))
+            self.ad = None
 
     @run_on_ui_thread
     def show_ad(self):
-        if self.ad:
+        try:
+            if platform != "android":
+                Clipboard.copy("NOT ANDROID PLATFORM")
+                return
+
+            if not self.ad:
+                Clipboard.copy("AD OBJECT NONE")
+                return
+
             activity = autoclass(
                 'org.kivy.android.PythonActivity'
             ).mActivity
 
             self.ad.show(activity, RewardCallback())
-        else:
-            toast("Ad not ready")
+
+        except Exception as e:
+            Clipboard.copy("SHOW ERROR:\n" + str(e))
+
 
 RewardApp().run()
